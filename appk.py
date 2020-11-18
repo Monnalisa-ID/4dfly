afacba6e05584dd77383782b74dad70b35fe58bc
Monnalisa <42632514+Monnalisa-ID@users.noreply.github.com>
2020-11-18 20:33:28 +0700

    Update appk.py

--- appk.py
+++ appk.py
@@ -1,3 +1,5 @@
+#Ver 1.24
+#Hargai Creator :D
 import time,sys,random,os
 import requests
 from fake_useragent import UserAgent
@@ -31,51 +33,53 @@ for i in prolist2.splitlines():
     pl.append(i)
 
 def tri(i):
-   try:
-    if requests.get(my_url,proxies={"https":"https://"+i},timeout=0.4).status_code == 200:
-       print (i)
-       tro(i)
-   except:
-    pass
+    try:
+        if requests.get(my_url,proxies={"https":"https://"+i},timeout=0.4).status_code == 200:
+            print(i)
+            tro(i)
+    except:
+                pass
 
 def fres(i):
     i.refresh()
 
 
 
-
-opts = Options()
-opts.add_argument('--ignore-certificate-errors')
-opts.add_argument('--allow-running-insecure-content')
+def tro(i):
+    opts = Options()
+    opts.add_argument('--ignore-certificate-errors')
+    opts.add_argument('--allow-running-insecure-content')
 #opts.add_argument("--proxy-server='direct://'")
 #opts.add_argument("--proxy-bypass-list=*")
-opts.add_argument('--proxy-server=%s' % i)
-opts.add_argument("--start-maximized")
-opts.add_argument('--disable-gpu')
-opts.add_argument('--disable-dev-shm-usage')
-opts.add_argument('--no-sandbox')
-opts.add_argument("user-agent=" + user_agent)
-opts.add_extension('./buster_ext.crx')
-opts.add_argument('--ignore-ssl-erros')
-print ('OTW TUYUL BOSQUE....')
-driver = webdriver.Chrome(chrome_options=opts)
-driver.minimize_window()
-driver.get('http://raboninco.com/1sLv9')
-path = "/html/body/div[6]/table/tbody/tr[1]/td/div/div[1]/span[2]/a/img"
-delay = 10 #Delay
-try:
-    element = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, path)))
-    element.click()
-    os.system('cls')
-    print ('OTW MENUYUL BOSQUE....')
-    time.sleep(2)
-finally:
-    print('MISI SELESAI BOSQUE...')
-    time.sleep(3)
-    os.system('cls')
-    driver.quit()
+    opts.add_argument('--proxy-server=%s' % i)
+    opts.add_argument("--start-maximized")
+    opts.add_argument('--disable-gpu')
+    opts.add_argument('--disable-dev-shm-usage')
+    opts.add_argument('--no-sandbox')
+    opts.add_argument("user-agent=" + user_agent)
+    opts.add_extension('./buster_ext.crx')
+    opts.add_argument('--ignore-ssl-erros')
+    print ('OTW TUYUL BOSQUE....')
+    driver = webdriver.Chrome(chrome_options=opts)
+    driver.minimize_window()
+    driver.get('http://raboninco.com/1sLv9')
+    path = "/html/body/div[6]/table/tbody/tr[1]/td/div/div[1]/span[2]/a/img"
+    delay = 10 #Delay
+
+def key_button():
+    try:
+        element = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, path)))
+        element.click()
+        os.system('cls')
+        print ('OTW MENUYUL BOSQUE....')
+        time.sleep(2)
+    finally:
+        print('MISI SELESAI BOSQUE...')
+        time.sleep(3)
+        os.system('cls')
+        driver.quit()
+
 tp = ThreadPool(5000)
 tp.map(tri,pl)
-
 tp = ThreadPool(300)
 tp.map(fres,dl)
